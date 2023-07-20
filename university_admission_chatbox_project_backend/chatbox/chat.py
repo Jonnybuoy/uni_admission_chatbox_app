b1478013b757685f0cb01288ed2071718bee1ec8
kaguithiajohnson@gmail.com <kaguithiajohnson@gmail.com>
2023-07-20 17:25:48 +0300

    Fetch responses for university search and admission requirements.

--- university_admission_chatbox_project_backend/chatbox/chat.py
+++ university_admission_chatbox_project_backend/chatbox/chat.py
@@ -27,7 +27,7 @@ model.eval()
 
 bot_name = "Unichatty"
 
-api_url = "https://api.data.gov/ed/collegescorecard/v1/schools/?school.operating=1&2015.student.size__range=1..&school.degrees_awarded.predominant__range=1..3&school.degrees_awarded.highest__range=2..4&api_key=I5gr1WgKH4UfV87xbbRK6vcwqlKrq4YPPL1C7eTx"
+api_url = "https://api.data.gov/ed/collegescorecard/v1/schools/?school.operating=1&2020.student.size__range=1..&school.degrees_awarded.predominant__range=1..3&school.degrees_awarded.highest__range=2..4&api_key=I5gr1WgKH4UfV87xbbRK6vcwqlKrq4YPPL1C7eTx"
 
 def fetch_university_info():
     try:
@@ -41,6 +41,31 @@ def fetch_university_info():
         print(f"An error occurred while fetching university data: {e}")
     
     return []
+
+def get_admission_requirements(university_data, sentence):
+    for result in university_data:
+        school_data = result.get('2020', {}).get('school', {})
+        school_data = school_data.get('name').split()
+        for name in school_data:
+            if name in sentence:
+                admissions_data = result.get('2020', {}).get('admissions', {})
+                admission_requirements = {
+                    'test_requirements': admissions_data.get('test_requiremetns'),
+                    'admission_rate': admissions_data.get('admission_rate'),
+                    'act_scores': admissions_data.get('act_scores'),
+                    'sat_scores': admissions_data.get('sat_scores')
+                }
+                response = f"{admission_requirements}"
+                return response
+
+def search_universities(university_data):
+    for result in university_data:
+        school_data = result.get('2020', {}).get('school', {})
+        school_name = school_data.get('name')
+        if school_name:
+            all_schools.append(school_name)
+        response = f"{all_schools}"
+    return response
 
 def get_response(msg):
     sentence = tokenize(msg)
@@ -56,18 +81,19 @@ def get_response(msg):
     probs = torch.softmax(output, dim=1)
     prob = probs[0][predicted.item()]
     
+    
     university_data = fetch_university_info()
+    all_schools = []
     
     if prob.item() > 0.75:
         for intent in intents['intents']:
             if tag == intent["tag"]:
                 if intent["tag"] == "search_universities":
-                    for uni_data in university_data:
-                        response = f"{uni_data['2015']['school']['name']}"
-                        return response
-                
-                
-    
+                    response = search_universities(university_data)
+
+                elif intent["tag"] == "admission_requirements":
+                    response = get_admission_requirements(university_data, sentence)
+
     return "I do not understand..."
 
 
